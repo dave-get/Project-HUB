1700586f0474ec48cb94495743291d3cfe7868be
Dawit Getachew <davewaanofii@gmail.com>
2025-05-18 15:21:01 +0300

    typo error

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,44 +1,49 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        fullName:{
-            type: String,
-            required: true,
-        },
-        email:{
-            type: String,
-            required: true,
-            unique: true
-        },
-        password:{
-            type: String,
-            required: true,
-            minlength: 6
-        },
-        role:{
-            type: String,
-            required: true,
-            enum: ['student', 'teacher', 'admin']
-        },
-        // Student specific fields
-        department: String,
-        bio: String,
-        imageUrl: {
-            type: String,
-            default: "default-profile.jpg"
-        },
-        phone: String,
-        location: String,
-        socialLinks: [{
-            platform: String,
-            url: String
-        }],
-        skills: {
-            type: string
-        }
-    },{ timestamps: true }
-)
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["student", "teacher", "admin"],
+    },
+    // Student specific fields
+    department: String,
+    bio: String,
+    imageUrl: {
+      type: String,
+      default: "default-profile.jpg",
+    },
+    phone: String,
+    location: String,
+    socialLinks: [
+      {
+        platform: String,
+        url: String,
+      },
+    ],
+    skills: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User",userSchema)
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
