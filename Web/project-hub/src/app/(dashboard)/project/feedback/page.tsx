597c9eadc28c1f4b964fe51daa644de3213afe72
Nathanael Wondwosen <nathanael4wof@gmail.com>
2025-05-23 09:24:39 +0300

    UI of the project feedback

---
+++ Web/project-hub/src/app/(dashboard)/project/feedback/page.tsx
@@ -0,0 +1,9 @@
+import FeedbackForm from "@/components/project-feedback-component/ProjectFeedback"
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-6 max-w-4xl">
+      <FeedbackForm />
+    </main>
+  )
+}
