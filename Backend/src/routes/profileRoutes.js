8c660d1b40f65211f3323ae52a28a71b2c3bc7eb
Dawit Getachew <davewaanofii@gmail.com>
2025-05-31 09:30:18 +0300

    route update

--- Backend/src/routes/profileRoutes.js
+++ Backend/src/routes/profileRoutes.js
@@ -3,14 +3,14 @@ import {
   updateUserProfile,
   getAllUserProfiles,
   getUserById,
-  uploadProfileImage
+  uploadProfileImage,
 } from "../controllers/userProfileController.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // Protect all routes
-
+router.use(protectRoute);
 
 // Get all User profiles
 router.get("/", getAllUserProfiles);
