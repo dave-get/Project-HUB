2c34e57f211a7c39c3453f24a54789f899191db6
tinsae316 <tinsaeteferi100@gmail.com>
2025-06-01 19:03:36 +0300

    updated

--- Web/project-hub/src/features/projectSubmitApi/projectSubmitApi.ts
+++ Web/project-hub/src/features/projectSubmitApi/projectSubmitApi.ts
@@ -1,15 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseQuery } from "@/lib/baseQuery";
 import Cookies from "js-cookie";
-<<<<<<< HEAD
 import { PROJECT_SUBMIT_ROUTES } from "@/config/api.config";
 import { ProjectFormValues } from "@/components/project-submission-component/schemas/project";
 import { ProjectSubmissionResponse } from "@/type/project";
-=======
-import { PROJECT_ROUTES } from "@/config/api.config";
-import { ProjectFormValues } from "@/components/project-submission-component/schemas/project";
-import { Project } from "@/type/project";
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
 
 // Define the project submission response type
 
@@ -19,19 +13,11 @@ export const projectSubmitApi = createApi({
   tagTypes: ["ProjectSubmission"],
   endpoints: (builder) => ({
     // Create new project submission
-<<<<<<< HEAD
     submitProject: builder.mutation<ProjectSubmissionResponse, FormData>({
       query: (data) => {
         console.log('Submitting project to:', PROJECT_SUBMIT_ROUTES.SUBMIT);
         return {
           url: PROJECT_SUBMIT_ROUTES.SUBMIT,
-=======
-    submitProject: builder.mutation<Project, FormData>({
-      query: (data) => {
-        // console.log('Submitting project to:', PROJECT_ROUTES.SUBMIT);
-        return {
-          url: PROJECT_ROUTES.SUBMIT,
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
           method: "POST",
           body: data,
           token: Cookies.get("access_token"),
--- Web/project-hub/src/type/project.ts
+++ Web/project-hub/src/type/project.ts
@@ -7,22 +7,14 @@ export interface TeamMember {
 export interface AppPlatform {
   title: string;
   description: string;
-<<<<<<< HEAD
-  logo: File;
-=======
   logo: string;
   link?: string;
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
 }
 
 export interface Tool {
   name: string;
   description: string;
-<<<<<<< HEAD
-  image: File;
-=======
   image: string;
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
 }
 
 // Tools and Machines
@@ -44,11 +36,6 @@ export type CodeAndDocumentation = {
   documentation: Documentation;
 };
 
-<<<<<<< HEAD
-// Main Project Submission Response
-export type ProjectSubmissionResponse = {
-  id: string;
-=======
 //
 export type Comment = {
   commenterId: string;
@@ -62,7 +49,6 @@ export type Comment = {
 // Main Project Submission Response
 export type Project = {
   _id: string;
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
   title: string;
   tags: string[];
   coverImage: string;
@@ -74,11 +60,6 @@ export type Project = {
   codeAndDocumentation: CodeAndDocumentation;
   status: boolean;
   reviewedByTeacherId?: string;
-<<<<<<< HEAD
-  createdAt: string;
-  updatedAt: string;
-};
-=======
   comments: Comment[];
   likes: [string];
   views: number;
@@ -89,4 +70,3 @@ export type Project = {
 export type Projects = {
   projects: Project[];
 };
->>>>>>> 6a3efad20f050be25ea398baffa86831bd787527
